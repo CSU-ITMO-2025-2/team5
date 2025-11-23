f022684ac083237362bedb909172aa5a6fc2cc3f
Semyon Mynko <samynko@MacBook-Pro-Semyon.local>
2025-11-23 20:23:16 +0300

    test

--- auth_service/app.py
+++ auth_service/app.py
@@ -7,8 +7,8 @@ from models import User
 from sqlalchemy.future import select
 
 app = FastAPI(
-    docs_url="/auth/docs",
-    openapi_url="/auth/openapi.json",
+    docs_url="/docs",
+    openapi_url="/openapi.json",
     servers=[{"url": "/auth"}]
 )
 
